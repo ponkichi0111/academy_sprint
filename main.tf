d06ee2ae9f96f6d5c8a2ade9a8f4735e9e7f7e23
財津宏幸 <zaitsuhiroyuki@zaitsuhiroyukinoMacBook-Air.local>
2025-01-20 00:11:19 +0900

    RDS作成

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ resource "aws_vpc" "reservation-vpc" {
 resource "aws_subnet" "web-subnet-01" {
   vpc_id     = aws_vpc.reservation-vpc.id
   cidr_block = "10.0.0.0/24"
+  availability_zone = "ap-northeast-1a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -23,10 +24,42 @@ resource "aws_subnet" "web-subnet-01" {
 resource "aws_subnet" "api-subnet-01" {
   vpc_id     = aws_vpc.reservation-vpc.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1a"
   map_public_ip_on_launch = true
   
   tags = {
     Name = "api-subnet-01"
+  }
+}
+
+resource "aws_subnet" "db-subnet-01" {
+  vpc_id     = aws_vpc.reservation-vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+  
+  tags = {
+    Name = "db-subnet-01"
+  }
+}
+
+resource "aws_subnet" "db-subnet-02" {
+  vpc_id     = aws_vpc.reservation-vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "ap-northeast-1c"
+  map_public_ip_on_launch = false
+  
+  tags = {
+    Name = "db-subnet-02"
+  }
+}
+
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name       = "db_subnet_group"
+  subnet_ids = [aws_subnet.db-subnet-01.id, aws_subnet.db-subnet-02.id]
+
+  tags = {
+    Name = "db_subnet_group"
   }
 }
 
@@ -107,12 +140,44 @@ resource "aws_security_group" "web-api-sg" {
   }
 }
 
+resource "aws_security_group" "db-sg" {
+  name        = "db-sg"
+  description = "Allow HTTP/SSH access"
+  vpc_id      = aws_vpc.reservation-vpc.id
+
+  ingress {
+    description = "RDS"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.web-api-sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  tags = {
+      Name = "db-sg"
+  }
+}
+
 resource "aws_instance" "api-server-01" {
   ami           = "ami-0fb04413c9de69305"
   instance_type = "t2.micro"
   security_groups = [aws_security_group.web-api-sg.id]
   subnet_id     = aws_subnet.api-subnet-01.id
   key_name      = "tf-key"
+
+  # コンソールから停止しても再構築されないようにパブリックIPを無視する
+  lifecycle {
+    ignore_changes = [
+      associate_public_ip_address
+    ]
+  }
 
   tags = {
       Name = "api-server-01"
@@ -126,7 +191,27 @@ resource "aws_instance" "web-server-01" {
   subnet_id     = aws_subnet.web-subnet-01.id
   key_name      = "tf-key"
 
+  # コンソールから停止しても再構築されないようにパブリックIPを無視する
+  lifecycle {
+    ignore_changes = [
+      associate_public_ip_address
+    ]
+  }
+
   tags = {
       Name = "web-server-01"
   }  
+}
+
+resource "aws_db_instance" "rds" {
+  db_name              = "reservationdb"
+  allocated_storage    = 20
+  engine               = "mysql"
+  instance_class       = "db.t3.micro"
+  username             = "admin"
+  password             = "passw0rd1234"
+  vpc_security_group_ids = [aws_security_group.db-sg.id]
+  db_subnet_group_name = aws_db_subnet_group.db_subnet_group.name
+
+  skip_final_snapshot = true
 }
